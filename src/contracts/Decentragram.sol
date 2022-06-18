a9d55848c4e718eebe8ebe0b28286ea29fc6e97f
Kaashyap <mskrishna.kaashyap@gmail.com>
2022-06-18 13:37:49 -0400

    add solidity code

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -1,5 +1,82 @@
-pragma solidity ^0.5.0;
+pragma solidity >=0.6.0;
 
 contract Decentragram {
-  // Code goes here...
+    // Code goes here...
+    string public name = "Decentragram";
+
+    uint256 public imageCount = 0;
+    struct Image {
+        uint256 id;
+        string hash;
+        string description;
+        uint256 tipAmount;
+        address payable author;
+    }
+
+    event ImageCreated(
+        uint256 id,
+        string hash,
+        string description,
+        uint256 tipAmount,
+        address payable author
+    );
+
+    event ImageTipped(
+        uint256 id,
+        string hash,
+        string description,
+        uint256 tipAmount,
+        address payable author
+    );
+
+    mapping(uint256 => Image) public images;
+
+    function uploadImage(string memory _imageHash, string memory _description)
+        public
+    {
+        require(bytes(_imageHash).length > 0, "Invalid Image Hash");
+        require(bytes(_description).length > 0, "Invalid Description");
+
+        require(msg.sender != address(0x0), "Invalid Sender");
+
+        imageCount++;
+
+        address payable _sender = payable(msg.sender);
+        images[imageCount] = Image(
+            imageCount,
+            _imageHash,
+            _description,
+            0,
+            _sender
+        );
+
+        emit ImageCreated(
+            imageCount,
+            _imageHash,
+            _description,
+            0,
+            payable(msg.sender)
+        );
+    }
+
+    function tipImageOwner(uint256 _id) public payable {
+        require(_id > 0 && _id <= imageCount);
+        Image memory _image = images[_id];
+
+        address payable _author = _image.author;
+
+        _author.transfer(msg.value);
+
+        _image.tipAmount += msg.value;
+
+        images[_id] = _image;
+
+        emit ImageTipped(
+            _id,
+            _image.hash,
+            _image.description,
+            msg.value,
+            payable(msg.sender)
+        );
+    }
 }
